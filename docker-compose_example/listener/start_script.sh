434b4d3ec71f9d08d63139c7dbdd1ddef36ac6f6
martinjakobsgaard <martin.jakobsgaard@gmail.com>
2019-04-08 11:40:00 +0200

    Got own example working with docker-compose, master, listener and talker.

--- docker-compose_example/listener/start_script.sh
+++ docker-compose_example/listener/start_script.sh
@@ -2,5 +2,6 @@
 
 #Author: martinjakobsgaard
 
+cd /root/catkin_ws
 source devel/setup.bash
 rosrun beginner_tutorials listener
--- docker-compose_example/talker/start_script.sh
+++ docker-compose_example/talker/start_script.sh
@@ -2,10 +2,6 @@
 
 #Author: martinjakobsgaard
 
-cd /root/catkin_ws \
-catkin_make \
-source devel/setup.bash \
-cd /root/catkin_ws/src \
-git clone https://github.com/martinjakobsgaard/ros_cont_test \
-cd /root/catkin_ws \
-catkin_make
+cd /root/catkin_ws
+source devel/setup.bash
+rosrun beginner_tutorials talker
