35fa113b6c5dbf6c2d0c79dcec12214af96b4a91
mathiaslyngbye <malyn16@student.sdu.dk>
2019-06-02 20:04:56 +0200

    Added arguments for local and remote

--- kubernetes/terminate_containers.sh
+++ kubernetes/terminate_containers.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-kubectl delete -f pod_gp_navfn_node_remote.yaml
-kubectl exec -it $(kubectl get pods -o=name | grep remote-gp | sed "s/^.\{4\}//") -c rosnode-gp -- bash -c "source root/catkin_ws/devel/setup.bash && rosnode kill global_planner"
-kubectl exec -it $(kubectl get pods -o=name | grep remote-gp | sed "s/^.\{4\}//") -c rosnode-alive -- bash -c "source root/catkin_ws/devel/setup.bash && rosnode kill alive"
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -r | --remote )
+            kubectl delete -f pod_gp_navfn_node_remote.yaml
+            kubectl exec -it $(kubectl get pods -o=name | grep remote-gp | sed "s/^.\{4\}//") -c rosnode-gp -- bash -c "source root/catkin_ws/devel/setup.bash && rosnode kill global_planner"
+            kubectl exec -it $(kubectl get pods -o=name | grep remote-gp | sed "s/^.\{4\}//") -c rosnode-alive -- bash -c "source root/catkin_ws/devel/setup.bash && rosnode kill alive"
+            exit
+            ;;
+        -l | --local )
+            kubectl delete -f pod_gp_navfn_node_local.yaml
+            kubectl exec -it $(kubectl get pods -o=name | grep rosnode-gp | sed "s/^.\{4\}//") -c rosnode-gp -- bash -c "source root/catkin_ws/devel/setup.bash && rosnode kill global_planner"
+            exit
+            ;;
+    esac
+done
